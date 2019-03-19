9000719d075027d448c64c2904bc0b3839d9b6b6
aki0207 <forever.look.aki@docomo.ne.jp>
2019-03-19 14:40:01 +0900

    写輪眼したhttps://qiita.com/yutat93/items/1b6dfe34fa8537cf3329

---
+++ QiitaView/ArticleListViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ArticleListViewController: UIViewController, UITableViewDataSource {
+    
+    var articles: [[String: String?]] = []
+    let table = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "記事一覧"
+        
+        table.frame = view.frame
+        view.addSubview(table)
+        table.dataSource = self
+        
+        getArticles()
+    }
+    
+    func getArticles() {
+        Alamofire.request("https://qiita.com/api/v2/items", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil)
+            .responseJSON { response in
+                guard let object = response.result.value else {
+                    return
+                }
+                
+                let json = JSON(object)
+                json.forEach { (_, json) in
+                    let article: [String: String?] = [
+                        "title": json["title"].string,
+                        "userId": json["user"]["id"].string
+                    ]
+                    self.articles.append(article)
+                }
+                self.table.reloadData()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article["title"]!
+        cell.detailTextLabel?.text = article["userId"]!
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    
+}
